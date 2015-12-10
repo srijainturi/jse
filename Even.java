5a5979cab3211c049d92e11cb737ca5a7eb018d3
Venki Vemula <Venki Vemula>
2015-12-09 21:37:55 -0800

    Even.java

---
+++ Even.java
@@ -0,0 +1,13 @@
+class Even
+{
+public static void main(String []ar)
+{
+  int i=5;
+  if(i%2==0)
+{
+  System.out.println("the given number is even");
+}
+else
+System.out.println("the given number is odd");
+}
+}
